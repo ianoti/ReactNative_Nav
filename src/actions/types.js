9609a1af1fbf66e8ea86d646574da78e01f40da0
ianoti <ianodhiambootieno@gmail.com>
2017-09-06 00:22:48 +0300

    make user authentication reducer more robust

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,4 @@
 export const EMAIL_CHANGED = 'email_has_changed';
 export const PASSWORD_CHANGED = 'password_has_changed';
 export const LOGIN_USER_SUCCESS = 'user_login_success';
+export const LOGIN_USER_FAIL = 'user_login_failed';
--- src/reducers/auth-reducer.js
+++ src/reducers/auth-reducer.js
@@ -1,9 +1,8 @@
 import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from '../actions/types';
 
-const INITIAL_STATE = { email: '', password: '' };
+const INITIAL_STATE = { email: '', password: '', user: null };
 
 export default (state = INITIAL_STATE, action) => {
-  console.log('action', action)
   switch (action.type) {
     case EMAIL_CHANGED:
       // use the spread operator to make a new copy of state and replace the email
@@ -12,7 +11,7 @@ export default (state = INITIAL_STATE, action) => {
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
     case LOGIN_USER_SUCCESS:
-      console.log('I reached');
+      return { ...state, user: action.payload };
     default:
       return state;
   }
